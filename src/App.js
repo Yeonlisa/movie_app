d53e5d70cd7aa6ec43d76f55600ba36c074f8b66
yeonlisa <tkzw4869@gmail.com>
2022-02-01 16:18:39 +0900

    Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import Potato from './Potato';
+
+function Food({ favorite }) {
+  return <h1>I like {favorite}</h1>
+}
 
 function App() {
   return (
     <div>
       <h1>Hello!!!</h1>
-      <Potato />
+      <Food favorite='kimchi' />
+      <Food favorite='ramen' />
+      <Food favorite='samgyeopsal' />
+      <Food favorite='chukumi' />
     </div>
   );
 }
