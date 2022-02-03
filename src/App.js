be82031a0eff81f071df5f34d5c2d4cddd646780
yeonlisa <tkzw4869@gmail.com>
2022-02-03 12:09:53 +0900

    Styling the Movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -23,10 +24,14 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : movies.map(movie => (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map(movie => (
               <Movie
                 key={movie.id}
                 id={movie.id}
@@ -35,8 +40,10 @@ class App extends React.Component {
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
               />
-          ))}
-      </div>
+            ))}
+          </div>
+        )}
+      </section>
     )
   }
 }
