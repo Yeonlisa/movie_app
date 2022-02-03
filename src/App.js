8a2eb8c9eccaf0f3e5add67422f59b558e046086
yeonlisa <tkzw4869@gmail.com>
2022-02-03 13:09:31 +0900

    오류 해결

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
   return (
     <HashRouter>
       <Navigation />
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/movie/:id" component={Detail} />
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/movie/:id" component={Detail} />
     </HashRouter>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -18,18 +18,18 @@ function Movie({ id, year, title, summary, poster, genres }) {
                 }
             }}
         >
-                    <img src={poster} alt={title} title={title} />
-                    <div className="movie__data">
-                    <h3 className="movie__title">{title}</h3>
-                    <h5 className="movie__year">{year}</h5>
-                    <ul className="movie__genres">
-                        {genres.map((genre, index) => (
-                        <li key={index} className="genres__genre">
-                            {genre}
-                        </li>
-                        ))}
-                    </ul>
-                    <p className="movie__summary">{summary.slice(0, 180)}...</p>
+            <img src={poster} alt={title} title={title} />
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="movie__genres">
+                    {genres.map((genre, index) => (
+                    <li key={index} className="genres__genre">
+                        {genre}
+                    </li>
+                    ))}
+                </ul>
+                <p className="movie__summary">{summary.slice(0, 180)}...</p>
                 </div>
             </Link>
         </div>
